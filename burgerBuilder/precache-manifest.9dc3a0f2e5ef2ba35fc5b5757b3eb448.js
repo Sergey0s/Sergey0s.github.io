d7cfccf04d03661662468f4106f05ab00d4b98c7
Sergey0s <59419109+Sergey0s@users.noreply.github.com>
2020-01-18 23:45:00 +0300

    Add files via upload

---
+++ burgerBuilder/precache-manifest.9dc3a0f2e5ef2ba35fc5b5757b3eb448.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d5a8ffe8773c788e1656df0ab25e485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2ef8229361e37c39053",
+    "url": "/static/css/3.3ef6c181.chunk.css"
+  },
+  {
+    "revision": "93bdae47a4b38c665e67",
+    "url": "/static/css/4.c6e56662.chunk.css"
+  },
+  {
+    "revision": "51fcacc29778d914be70",
+    "url": "/static/css/5.4fd5de8a.chunk.css"
+  },
+  {
+    "revision": "1a917ba8ae6d586e6ee7",
+    "url": "/static/css/main.c8023148.chunk.css"
+  },
+  {
+    "revision": "d4b646df97e32096e060",
+    "url": "/static/js/2.0cbb27bc.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.0cbb27bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2ef8229361e37c39053",
+    "url": "/static/js/3.209ef650.chunk.js"
+  },
+  {
+    "revision": "93bdae47a4b38c665e67",
+    "url": "/static/js/4.98d0acbd.chunk.js"
+  },
+  {
+    "revision": "51fcacc29778d914be70",
+    "url": "/static/js/5.9a0f765a.chunk.js"
+  },
+  {
+    "revision": "1a917ba8ae6d586e6ee7",
+    "url": "/static/js/main.cedd495a.chunk.js"
+  },
+  {
+    "revision": "8438cd363e16889d5773",
+    "url": "/static/js/runtime-main.841a25a1.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/27.1 burger-logo.png.b8503d26.png"
+  }
+]);
